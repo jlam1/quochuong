675d2675b3fdf51969985a4fd293f5848ba3988c
John Lam <johnlam2478@gmail.com>
2019-01-16 12:26:31 -0500

    vue analytics add, build, replace index with under maintenance

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAnalytics from 'vue-analytics'
+
 import './../node_modules/bulma/css/bulma.min.css';
 import './../node_modules/lightbox2/src/css/lightbox.css';
 
 import './../node_modules/jquery/dist/jquery.min.js';
 import './../node_modules/lightbox2/src/js/lightbox.js';
+
+
+Vue.use(VueAnalytics, {
+  id: 'UA-132525741-1',
+  checkDuplicatedScript: true
+})
 
 Vue.config.productionTip = false
 
