9ba0a64751a422f5735f78787ddf4b511a09d97d
John Lam <johnlam.dev@gmail.com>
2021-04-20 17:32:08 -0400

    new prices

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -3,17 +3,6 @@
 	<div class="splash-image">
 		<img src="../assets/images/headline_background.png" alt="quoc huong shop" title="by william pham">
 	</div>
-	<!-- <section class="home section">
-		<div class="container">
-			<h1 class="is-primary has-text-centered is-size-1 section-title line-through">Welcome!</h1>
-			<div class="content">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque architecto magni sapiente doloremque molestiae
-					quisquam ea nam necessitatibus, deleniti culpa minus sunt fugiat hic. Velit asperiores eius possimus quidem nulla
-					unde minus quis, dignissimos delectus excepturi natus nesciunt temporibus impedit? Atque adipisci est provident
-					aut repellat! Eius maxime et quasi?
-			</div>
-		</div>
-	</section> -->
 </div>
 </template>
 
